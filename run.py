d17dbf30aba271a536d58c78e1ce8d1c9fc8f54c
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-04-01 17:10:44 +0000

    Removed commented game legend and removed code to preview HIDDEN_BOARD and GUESS_BOARD

--- run.py
+++ run.py
@@ -3,10 +3,6 @@ Import random to generate random integers in game
 """
 from random import randint
 
-# Game Legend
-# ▢ - blank space
-# ⊗ - missed shot
-# ⊗ - hit ship
 
 HIDDEN_BOARD = [["▢"] * 8 for x in range(8)]
 GUESS_BOARD = [["▢"] * 8 for x in range(8)]
@@ -33,10 +29,6 @@ def print_board(board):
     for row in board:
         print("%d | %s | " % (row_number, " | ".join(row)))
         row_number += 1
-
-
-print_board(GUESS_BOARD)
-print_board(HIDDEN_BOARD)
 
 
 def create_ships(board):
