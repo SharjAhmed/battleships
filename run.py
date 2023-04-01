f5e8c660d27109afa732599de88d4c5c4890f8a0
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-04-01 00:07:05 +0000

    Create 8x8  game board

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Import random to generate random integers in game
+"""
+from random import randint
+
+# Game Legend
+# ▢ - blank space
+# ⨀ - missed shot
+# ⊗ - hit ship
+
+HIDDEN_BOARD = [["▢"] * 8 for x in range(8)]
+GUESS_BOARD = [["▢"] * 8 for x in range(8)]
+letters_to_numbers = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+}
+
+board = []
+
+
+def print_board(board):
+    """
+    Create board for player to guess where to hit
+    """
+    print("    A   B   C   D   E   F   G   H")
+    row_number = 1
+    for row in board:
+        print("%d | %s | " % (row_number, " | ".join(row)))
+        row_number += 1
+
+
+print_board(GUESS_BOARD)
+print_board(HIDDEN_BOARD)
